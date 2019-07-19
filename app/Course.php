72ee630b7a5ec5f58a01a7cb23981f72cb1014b7
Scott <scottwilkinson@Scotts-MacBook-Pro.local>
2019-07-19 16:21:24 -0700

    Working on wip and release status

--- app/Course.php
+++ app/Course.php
@@ -5,19 +5,29 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 define('RELEASE_NOTSET', 0);
-define('RELEASE_DRAFT', 1);
-define('RELEASE_REVIEW', 5);
-define('RELEASE_APPROVED', 9);
-define('RELEASE_PUBLISHED', 10);
+define('RELEASE_ADMIN', 10);
+define('RELEASE_DRAFT', 20);
+define('RELEASE_REVIEW', 30);
+define('RELEASE_APPROVED', 90);
+define('RELEASE_PUBLISHED', 100);
 
 define('WIP_NOTSET', 0);
-define('WIP_DEV', 1);
-define('WIP_TEST', 5);
-define('WIP_ONHOLD', 9);
-define('WIP_FINISHED', 10);
+define('WIP_ONHOLD', 10);
+define('WIP_DEV', 20);
+define('WIP_TEST', 30);
+define('WIP_FINISHED', 100);
 
 class Course extends Base
 {
+    static private $_releaseFlags = [
+			RELEASE_NOTSET => 'Not Set',
+			RELEASE_ADMIN => 'Admin Only',
+			RELEASE_DRAFT => 'Draft',
+			RELEASE_REVIEW => 'Review',
+			RELEASE_APPROVED => 'Approved',
+			RELEASE_PUBLISHED => 'Published',
+    ];
+
     public function user()
     {
     	return $this->belongsTo(User::class);
@@ -26,48 +36,49 @@ class Course extends Base
     static public function get($id)
     {
 		$id = intval($id);
-		
+
 		$record = Course::select()
 			->where('deleted_flag', 0)
 			->where('id', $id)
 			->first();
-			
+
 		return $record;
 	}
-	
+
     static public function getReleaseFlags()
     {
-		$v = [
-			RELEASE_NOTSET => 'Not Released',
-			RELEASE_DRAFT => 'Draft',
-			RELEASE_REVIEW => 'Review',
-			RELEASE_APPROVED => 'Approved',
-			RELEASE_PUBLISHED => 'Published',
-		];
-		
-		return $v;
+		return Course::$_releaseFlags;
 	}
-	
+
     static public function getWipFlags()
     {
 		$v = [
-			WIP_NOTSET => 'Not Started',
+			WIP_NOTSET => 'Not Set',
+			WIP_ONHOLD => 'On Hold',
 			WIP_DEV => 'Development',
 			WIP_TEST => 'Test',
-			WIP_ONHOLD => 'On Hold',
 			WIP_FINISHED => 'Finished',
 		];
-		
+
 		return $v;
-	}	
-	
+	}
+
     static public function getIndex($parms = [])
     {
 		$records = []; // make this countable so view will always work
-				
+        $p = count($parms) > 0 ? $parms[0] : '';
+//dd($parms);
 		if (Tools::isAdmin())
 		{
-			if (array_key_exists('unfinished', $parms))
+			if ($p == 'all')
+			{
+				$records = Course::select()
+	//				->where('site_id', SITE_ID)
+					->where('deleted_flag', 0)
+					->orderBy('display_order')
+					->get();
+			}
+			else if (array_key_exists('unfinished', $parms))
 			{
 				$records = Course::select()
 	//				->where('site_id', SITE_ID)
@@ -81,6 +92,7 @@ class Course extends Base
 				$records = Course::select()
 	//				->where('site_id', SITE_ID)
 					->where('deleted_flag', 0)
+					->where('wip_flag', '>', WIP_ONHOLD)
 					->orderBy('display_order')
 					->get();
 			}
@@ -90,41 +102,53 @@ class Course extends Base
 			$records = Course::select()
 //				->where('site_id', SITE_ID)
 				->where('deleted_flag', 0)
-				->where('release_flag', '>=', RELEASE_PUBLISHED)				
+				->where('release_flag', '>=', RELEASE_PUBLISHED)
 				->orderBy('display_order')
 				->get();
 		}
-		
+
 		return $records;
 	}
-	
+
     public function isPublished()
     {
     	return ($this->release_flag >= RELEASE_PUBLISHED);
     }
 
+    static public function safeArrayGetString($array, $key, $default)
+    {
+        $v = $default;
+
+        if (array_key_exists($key, $array))
+        {
+            $v = $array[$key];
+        }
+
+        return $v;
+    }
+
     public function getStatus()
     {
 		$text = '';
-		$color = '';
-		$done = true;
-		$btn = '';
-		
-		if ($this->release_flag < RELEASE_APPROVED)
+		$color = 'yellow';
+		$done = false;
+		$btn = 'btn-warning';
+		$releaseFlags = Course::getReleaseFlags();
+
+        if ($this->release_flag < RELEASE_APPROVED)
 		{
-			$text = 'Approve';
-			$color = 'yellow';
-			$btn = 'btn-warning';
-			$done = false;
+		    //$text = $releaseFlags[$this->release_flag];
+
+		    $text = Course::safeArrayGetString($releaseFlags, $this->release_flag, 'Not Found');
 		}
-		else if ($this->release_flag < RELEASE_PUBLISHED)
+		else
 		{
 			$text = 'Publish';
 			$color = 'green';
 			$btn = 'btn-success';
 			$done = false;
 		}
-		
+
     	return ['text' => $text, 'color' => $color, 'btn' => $btn, 'done' => $done];
     }
 }
--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -25,65 +25,65 @@ class CourseController extends Controller
 	public function __construct ()
 	{
         $this->middleware('is_admin')->except(['index', 'view', 'permalink']);
-		
+
 		$this->prefix = PREFIX;
 		$this->title = TITLE;
 		$this->titlePlural = TITLE_PLURAL;
-		
+
 		parent::__construct();
 	}
-	
+
     public function index(Request $request)
-    {	
+    {
 		$records = []; // make this countable so view will always work
-		
+
 		try
 		{
 			$records = Course::getIndex();
 		}
-		catch (\Exception $e) 
+		catch (\Exception $e)
 		{
 			$msg = 'Error getting ' . TITLE_LC . ' list';
 			Event::logException(LOG_MODEL, LOG_ACTION_SELECT, $msg, null, $e->getMessage());
 			Tools::flash('danger', $msg);
-		}	
-			
+		}
+
 		return view(PREFIX . '.index', $this->getViewData([
 			'records' => $records,
 		]));
-    }	
+    }
 
     public function admin(Request $request)
     {
 		$records = []; // make this countable so view will always work
-		
+
 		try
 		{
-			$records = Course::getIndex();
+			$records = Course::getIndex(['all']);
 		}
-		catch (\Exception $e) 
+		catch (\Exception $e)
 		{
 			$msg = 'Error getting ' . TITLE_LC . ' list';
 			Event::logException(LOG_MODEL, LOG_ACTION_SELECT, $msg, null, $e->getMessage());
 			Tools::flash('danger', $msg);
-		}	
-			
+		}
+
 		return view(PREFIX . '.admin', $this->getViewData([
 			'records' => $records,
 		]));
-    }	
-	
+    }
+
     public function add()
-    {		 
+    {
 		return view(PREFIX . '.add', $this->getViewData([
 			]));
 	}
-		
+
     public function create(Request $request)
-    {					
+    {
 		$record = new Course();
-		
-		$record->user_id 		= Auth::id();				
+
+		$record->user_id 		= Auth::id();
 		$record->title 			= $request->title;
 		$record->description	= $request->description;
 		$record->permalink		= Tools::createPermalink($request->title);
@@ -91,28 +91,28 @@ class CourseController extends Controller
 		try
 		{
 			$record->save();
-			
-			Event::logAdd(LOG_MODEL, $record->title, $record->site_url, $record->id);			
+
+			Event::logAdd(LOG_MODEL, $record->title, $record->site_url, $record->id);
 			Tools::flash('success', 'New ' . TITLE_LC . ' has been added');
 		}
-		catch (\Exception $e) 
+		catch (\Exception $e)
 		{
 			$msg = 'Error adding new ' . TITLE_LC;
 			Event::logException(LOG_MODEL, LOG_ACTION_ADD, $record->title, null, $msg . ': ' . $e->getMessage());
 			Tools::flash('danger', $msg);
 
-			return back(); 
-		}	
-			
+			return back();
+		}
+
 		return redirect(REDIRECT_ADMIN);
     }
 
     public function permalink(Request $request, $permalink)
     {
 		$permalink = trim($permalink);
-		
+
 		$record = null;
-			
+
 		try
 		{
 			$record = Course::select()
@@ -122,41 +122,41 @@ class CourseController extends Controller
 				->where('permalink', $permalink)
 				->first();
 		}
-		catch (\Exception $e) 
+		catch (\Exception $e)
 		{
 			$msg = 'Entry Not Found: ' . $permalink;
-			
-			Tools::flash('danger', $msg);			
+
+			Tools::flash('danger', $msg);
 			Event::logError(LOG_MODEL, LOG_ACTION_PERMALINK, /* title = */ $msg);
-			
-			return back();					
-		}	
+
+			return back();
+		}
 
 		return view(PREFIX . '.view', $this->getViewData([
-			'record' => $record, 
+			'record' => $record,
 			], LOG_MODEL, LOG_PAGE_PERMALINK));
 	}
-	
+
 	public function view(Course $course)
-    {		
+    {
 		$record = $course;
-		
+
 		$records = []; // make this countable so view will always work
 		try
 		{
 			$records = Lesson::getIndex($course->id);
 		}
-		catch (\Exception $e) 
-		{			
+		catch (\Exception $e)
+		{
 			$msg = 'Error getting lesson list';
 			Event::logException(LOG_MODEL, LOG_ACTION_VIEW, $msg, null, $e->getMessage());
-			Tools::flash('danger', $msg);			
+			Tools::flash('danger', $msg);
 		}
-		
+
 		// put some view helpers together
 		$disabled = (count($records) > 0) ? '' : 'disabled';
 		$firstId = (count($records) > 0) ? $records[0]->id : 0;
-		
+
 		return view(PREFIX . '.view', $this->getViewData([
 			'record' => $record,
 			'records' => $records,
@@ -164,42 +164,42 @@ class CourseController extends Controller
 			'firstId' => $firstId,
 			], LOG_MODEL, LOG_PAGE_VIEW));
     }
-	
+
 	public function edit(Course $course)
-    {		 
+    {
 		$record = $course;
-		
+
 		return view(PREFIX . '.edit', $this->getViewData([
 			'record' => $record,
 			]));
     }
-	
+
     public function update(Request $request, Course $course)
     {
-		$record = $course; 
-		 
+		$record = $course;
+
 		$isDirty = false;
 		$changes = '';
 
 		$record->title = Tools::copyDirty($record->title, $request->title, $isDirty, $changes);
 		$record->description = Tools::copyDirty($record->description, $request->description, $isDirty, $changes);
 		$record->display_order = Tools::copyDirty($record->display_order, $request->display_order, $isDirty, $changes);
-								
+
 		if ($isDirty)
-		{						
+		{
 			try
 			{
 				$record->save();
 
-				Event::logEdit(LOG_MODEL, $record->title, $record->id, $changes);			
-				Tools::flash('success', TITLE . ' has been updated');				
+				Event::logEdit(LOG_MODEL, $record->title, $record->id, $changes);
+				Tools::flash('success', TITLE . ' has been updated');
 			}
-			catch (\Exception $e) 
+			catch (\Exception $e)
 			{
 				$msg = 'Error updating ' . TITLE_LC;
 				Event::logException(LOG_MODEL, LOG_ACTION_EDIT, $record->title, null, $msg . ': ' . $e->getMessage());
 				Tools::flash('danger', $msg);
-			}				
+			}
 		}
 		else
 		{
@@ -208,41 +208,41 @@ class CourseController extends Controller
 
 		return redirect('/' . PREFIX . '/view/' . $record->id);
 	}
-	
+
     public function confirmdelete(Course $course)
-    {	
-		$record = $course; 
-	
+    {
+		$record = $course;
+
 		$vdata = $this->getViewData([
 			'record' => $record,
-		]);				
-		 
+		]);
+
 		return view(PREFIX . '.confirmdelete', $vdata);
     }
-	
+
     public function delete(Request $request, Course $course)
-    {	
-		$record = $course; 
-				
-		try 
+    {
+		$record = $course;
+
+		try
 		{
 			$record->deleteSafe();
-			Event::logDelete(LOG_MODEL, $record->title, $record->id);					
+			Event::logDelete(LOG_MODEL, $record->title, $record->id);
 			Tools::flash('success', $this->title . ' has been deleted');
 		}
-		catch (\Exception $e) 
+		catch (\Exception $e)
 		{
 			Event::logException(LOG_MODEL, LOG_ACTION_DELETE, $record->title, $record->id, $e->getMessage());
-			Tools::flash('danger', $e->getMessage());			
-		}	
-			
+			Tools::flash('danger', $e->getMessage());
+		}
+
 		return redirect(REDIRECT_ADMIN);
-    }	
-	
+    }
+
     public function undelete()
-    {	
+    {
 		$records = []; // make this countable so view will always work
-		
+
 		try
 		{
 			$records = Course::select()
@@ -250,50 +250,50 @@ class CourseController extends Controller
 				->where('deleted_flag', 1)
 				->get();
 		}
-		catch (\Exception $e) 
+		catch (\Exception $e)
 		{
 			$msg = 'Error getting ' . TITLE_LC . ' list';
 			Event::logException(LOG_MODEL, LOG_ACTION_UNDELETE, $msg, null, $e->getMessage());
 			Tools::flash('danger', $msg);
-		}	
-			
+		}
+
 		return view(PREFIX . '.undelete', $this->getViewData([
 			'records' => $records,
-		]));		
+		]));
     }
 
     public function publish(Request $request, Course $course)
-    {			
-		$record = $course; 
-	
+    {
+		$record = $course;
+
 		return view(PREFIX . '.publish', $this->getViewData([
 			'record' => $record,
 			'release_flags' => Course::getReleaseFlags(),
 			'wip_flags' => Course::getWipFlags(),
 		]));
     }
-	
+
     public function publishupdate(Request $request, Course $course)
-    {	
-		$record = $course; 
+    {
+		$record = $course;
 
 		$record->release_flag = $request->release_flag;
 		$record->wip_flag = $request->wip_flag;
-		
+
 		try
 		{
 			$record->save();
-			
-			Event::logEdit(LOG_MODEL, $record->title, $record->id, 'Release/work status updated');			
+
+			Event::logEdit(LOG_MODEL, $record->title, $record->id, 'Release/work status updated');
 			Tools::flash('success', $this->title . ' status has been updated');
 		}
-		catch (\Exception $e) 
+		catch (\Exception $e)
 		{
 			$msg = 'Error updating ' . TITLE_LC . ' status';
 			Event::logException(LOG_MODEL, LOG_ACTION_PUBLISH, $record->title, null, $e->getMessage());
 			Tools::flash('danger', $msg);
-		}				
-		
+		}
+
 		return redirect(REDIRECT_ADMIN);
-    }	
+    }
 }
