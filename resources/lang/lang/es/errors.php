bcde801592b8bbb633e7d498eab8a49cdb723e67
scottwestseattle <sbwilkinson@yahoo.com>
2020-05-27 18:52:42 -0700

    Latest translations

---
+++ resources/lang/lang/es/errors.php
@@ -0,0 +1,4 @@
+<?php
+return [
+'Login Error: User is Blocked' => 'Error de inicio de sesión: el usuario está bloqueado',
+];
---
+++ resources/lang/lang/es/gen.php
@@ -0,0 +1,13 @@
+<?php
+return [
+'Title' => 'Título',
+'Description' => 'Descripción',
+'Text' => 'Texto',
+'Start' => 'Comenzar',
+'Finished' => 'Terminado',
+'Main Photo' => 'Foto principal',
+'Seconds' => 'Segundos',
+'Break Seconds' => 'Segundos de Descanso',
+'Reps' => 'Repeticiones',
+'Chapter Title' => 'Título del capitulo',
+];
